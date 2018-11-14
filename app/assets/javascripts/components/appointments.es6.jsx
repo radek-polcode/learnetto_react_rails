409af73a36d75137bb794996ed0c2be73601eb21
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-11-14 11:57:49 +0100

    Fix for datetime + changes in component

--- app/assets/javascripts/components/appointments.es6.jsx
+++ app/assets/javascripts/components/appointments.es6.jsx
@@ -1,5 +1,6 @@
+
 class Appointments extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
       appointments: this.props.appointments,
@@ -8,41 +9,35 @@ class Appointments extends React.Component {
     }
   }
 
-  handleUserInput(obj) {
+  handleUserInput (obj) {
     this.setState(obj);
   }
 
-  handleFormSubmit() {
-    var appointment = {
-                        title: this.state.title,
-                        appt_time: this.state.appt_time
-                      };
-    $.post(
-            '/appointments',
-            { appointment: appointment }
-          ).done(function(data) {
+  handleFormSubmit () {
+    const appointment = {title: this.state.title, appt_time: this.state.appt_time};
+    $.post('/appointments',
+            {appointment: appointment})
+          .done((data) => {
             this.addNewAppointment(data);
-          }.bind(this));
-  }
-  addNewAppointment() {
-    var appointments = React.addons.update(
-                                      this.state.appointments,
-                                      { $push: [appointments] }
-                                    )
-    this.setState({
-      appointments: appointments.sort(function(a,b) {
-        return new Date(a.appt_time) - new Date(b.appt_time)
-      })
-    })
+          });
   }
 
-  render() {
-    return(
+  addNewAppointment (appointment) {
+    const appointments = React.addons.update(this.state.appointments, { $push: [appointment]});
+    this.setState({
+      appointments: appointments.sort(function(a,b){
+        return new Date(a.appt_time) - new Date(b.appt_time);
+      })
+    });
+  }
+
+  render () {
+    return (
       <div>
         <AppointmentForm input_title={this.state.title}
-                         input_appt_time={this.state.appt_time}
-                         onUserInput={this.handleUserInput.bind(this)}
-                         onFormSubmit={this.handleFormSubmit} />
+          input_appt_time={this.state.appt_time}
+          onUserInput={(obj) => this.handleUserInput(obj)}
+          onFormSubmit={() => this.handleFormSubmit()} />
         <AppointmentsList appointments={this.state.appointments} />
       </div>
     )
