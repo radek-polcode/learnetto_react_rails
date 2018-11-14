08ba4821b2092bf90dac45ced2b2b3d48bd2b5ea
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-11-14 09:34:15 +0100

    Change appointment component to ES6

--- app/assets/javascripts/components/appointment.es6.jsx
+++ app/assets/javascripts/components/appointment.es6.jsx
@@ -1,10 +1,10 @@
-var Appointment = createReactClass({
-  render: function() {
+class Appointment extends React.Component {
+  render() {
     return (
-      <div key={this.props.appointment.id}>
+      <div className='appointment'>
         <h3>{this.props.appointment.title}</h3>
         <p>{formatDate(this.props.appointment.appt_time)}</p>
       </div>
     )
   }
-});
+}
