1c8e869fca10594d48143697db3202325eb95ad7
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-11-19 10:04:14 +0100

    Add validation for appt time and title length

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,3 +1,13 @@
 class Appointment < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, length: { minimum: 3 }
+  validates :appt_time, presence: true
+
+  validate :appt_time_cannot_be_in_the_past
+
+  private
+  def appt_time_cannot_be_in_the_past
+    if appt_time.present? && appt_time < Time.now
+      errors.add(:appt_time, "cant't be in the past")
+    end
+  end
 end
