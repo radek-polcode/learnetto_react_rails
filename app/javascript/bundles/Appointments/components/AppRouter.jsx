dc3c397225aec182e2a62bd34e00bea005cd1e27
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-11-21 09:15:58 +0100

    Add AppHeader component

--- app/javascript/bundles/Appointments/components/AppRouter.jsx
+++ app/javascript/bundles/Appointments/components/AppRouter.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Appointments from './Appointments';
 import Appointment from './Appointment';
+import { AppHeader } from './AppHeader';
 
 export default (props) => {
   return (
     <Router>
       <div>
+        <AppHeader />
         <Route exact path="/" render={routeProps => (
           <Appointments {...routeProps} appointments={props.appointments} />
         )} />
