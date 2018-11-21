3752c92f1a450c808f4b59db34c9fa734db7028f
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2018-11-21 12:13:58 +0100

    Move code from Appointments to AppointmentForm

--- app/javascript/bundles/Appointments/components/AppointmentForm.jsx
+++ app/javascript/bundles/Appointments/components/AppointmentForm.jsx
@@ -3,20 +3,12 @@ import Datetime from 'react-datetime';
 import moment from 'moment';
 import { validations } from '../utils/validations'
 import PropTypes from 'prop-types';
+import { FormErrors } from './FormErrors';
+import update from 'immutability-helper';
 
 export default class AppointmentForm extends React.Component {
   static propTypes = {
-    formValid: PropTypes.bool.isRequired,
-    title: PropTypes.shape({
-      value: PropTypes.string.isRequired,
-      valid: PropTypes.bool.isRequired
-    }).isRequired,
-    appt_time: PropTypes.shape({
-      value: PropTypes.instanceOf(Date).isRequired,
-      valid: PropTypes.bool.isRequired
-    }),
-    onUserInput: PropTypes.func.isRequired,
-    onFormSubmit: PropTypes.func.isRequired
+    handleNewAppointment: PropTypes.func
   }
 
   static formValidations = {
@@ -28,25 +20,93 @@ export default class AppointmentForm extends React.Component {
     ]
   }
 
+  constructor (props, railsContext) {
+    super(props)
+    this.state = {
+      appointments: this.props.appointments,
+      title: { value: '', valid: false },
+      appt_time: { value: new Date(), valid: false },
+      formErrors: {},
+      formValid: false
+    }
+  }
+
+  handleUserInput = (fieldName, fieldValue, validations) => {
+    const newFieldState = update(this.state[fieldName],
+                                 {value: {$set: fieldValue}});
+    this.setState(
+      {[fieldName]: newFieldState}, 
+      () => { this.validateField(fieldName, fieldValue, validations) }
+    );
+  }
+
+  validateField(fieldName, fieldValue, validations) {
+    let fieldValid;
+    let fieldErrors = validations.reduce((errors, v) => {
+      let e = v(fieldValue);
+      if(e !== '') {
+        errors.push(e)
+      }
+      return(errors);
+    }, []);
+
+    fieldValid = fieldErrors.length === 0;
+
+    const newFieldState = update(this.state[fieldName],
+                                {valid: {$set: fieldValid}});
+
+    const newFormErrors = update(this.state.formErrors,
+                                 {$merge: {[fieldName]: fieldErrors}})
+    this.setState(
+      {[fieldName]: newFieldState,
+      formErrors: newFormErrors}, 
+      this.validateForm
+    );
+  }
+
+  validateForm() {
+    this.setState(
+      { formValid: this.state.title.valid &&
+                   this.state.appt_time.valid }
+    );
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    const appointment = {
+      title: this.state.title.value, 
+      appt_time: this.state.appt_time.value
+    };
+
+    $.post('/appointments',
+            {appointment: appointment})
+          .done((data) => {
+            this.props.handleNewAppointment(data);
+            this.resetFormErrors();
+          })
+          .fail((response) => {
+            this.setState({ formErrors: response.responseJSON });
+          });
+  }
+
+  resetFormErrors() {
+    this.setState({formErrors: {}});
+  }
+
   handleChange = (e) => {
     const fieldName = e.target.name;
     const fieldValue = e.target.value;
-    this.props.onUserInput(
-                fieldName, 
-                fieldValue, 
-                AppointmentForm.formValidations[fieldName]
-              );
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.props.onFormSubmit();
+    this.handleUserInput(
+          fieldName, 
+          fieldValue, 
+          AppointmentForm.formValidations[fieldName]
+        );
   }
 
   setApptTime = (e) => {
     const fieldName = 'appt_time';
     const fieldValue = e.toDate();
-    this.props.onUserInput(
+    this.handleUserInput(
                 fieldName, 
                 fieldValue,
                 AppointmentForm.formValidations[fieldName]
@@ -61,20 +121,21 @@ export default class AppointmentForm extends React.Component {
     return(
       <div>
         <h2>Make a new appointment</h2>
-        <form onSubmit={this.handleSubmit}>
+        <FormErrors formErrors={this.state.formErrors} />
+        <form onSubmit={this.handleFormSubmit}>
           <input name='title' 
                 placeholder='Appointment Title'
-                value={this.props.title.value}
+                value={this.state.title.value}
                 onChange={this.handleChange} />
           <Datetime input={false} 
                     open={true} 
                     inputProps={inputProps}
-                    value={moment(this.props.appt_time.value)}
+                    value={moment(this.state.appt_time.value)}
                     onChange={this.setApptTime} />
           <input type='submit' 
                  value='Make Appointment'
                  className='submit-button'
-                 disabled={!this.props.formValid} />
+                 disabled={!this.state.formValid} />
         </form>
       </div>
     )
--- app/javascript/bundles/Appointments/components/Appointments.jsx
+++ app/javascript/bundles/Appointments/components/Appointments.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import AppointmentForm from './AppointmentForm';
 import { AppointmentsList } from './AppointmentsList';
 import update from 'immutability-helper';
-import { FormErrors } from './FormErrors';
-import moment from 'moment';
 import PropTypes from 'prop-types';
 
 export default class Appointments extends React.Component {
@@ -19,76 +16,11 @@ export default class Appointments extends React.Component {
   constructor (props, railsContext) {
     super(props)
     this.state = {
-      appointments: this.props.appointments,
-      title: { value: '', valid: false },
-      appt_time: { value: new Date(), valid: false },
-      formErrors: {},
-      formValid: false
+      appointments: this.props.appointments
     }
   }
 
-  handleUserInput = (fieldName, fieldValue, validations) => {
-    const newFieldState = update(this.state[fieldName],
-                                 {value: {$set: fieldValue}});
-    this.setState(
-      {[fieldName]: newFieldState}, 
-      () => { this.validateField(fieldName, fieldValue, validations) }
-    );
-  }
-
-  validateField(fieldName, fieldValue, validations) {
-    let fieldValid;
-    let fieldErrors = validations.reduce((errors, v) => {
-      let e = v(fieldValue);
-      if(e !== '') {
-        errors.push(e)
-      }
-      return(errors);
-    }, []);
-
-    fieldValid = fieldErrors.length === 0;
-
-    const newFieldState = update(this.state[fieldName],
-                                {valid: {$set: fieldValid}});
-
-    const newFormErrors = update(this.state.formErrors,
-                                 {$merge: {[fieldName]: fieldErrors}})
-    this.setState(
-      {[fieldName]: newFieldState,
-      formErrors: newFormErrors}, 
-      this.validateForm
-    );
-  }
-
-  validateForm() {
-    this.setState(
-      { formValid: this.state.title.valid &&
-                   this.state.appt_time.valid }
-    );
-  }
-
-  handleFormSubmit = () => {
-    const appointment = {
-      title: this.state.title.value, 
-      appt_time: this.state.appt_time.value
-    };
-
-    $.post('/appointments',
-            {appointment: appointment})
-          .done((data) => {
-            this.addNewAppointment(data);
-            this.resetFormErrors();
-          })
-          .fail((response) => {
-            this.setState({ formErrors: response.responseJSON });
-          });
-  }
-
-  resetFormErrors() {
-    this.setState({formErrors: {}});
-  }
-
-  addNewAppointment (appointment) {
+  addNewAppointment = (appointment) => {
     const appointments = update(this.state.appointments, { $push: [appointment]});
     this.setState({
       appointments: appointments.sort(function(a,b){
@@ -112,14 +44,7 @@ export default class Appointments extends React.Component {
   render () {
     return (
       <div>
-        <FormErrors formErrors={this.state.formErrors} />
-        <AppointmentForm 
-          formValid = {this.state.formValid}
-          title={this.state.title}
-          appt_time={this.state.appt_time}
-          onUserInput={this.handleUserInput}
-          onFormSubmit={this.handleFormSubmit} 
-        />
+        <AppointmentForm handleNewAppointment={this.addNewAppointment} />
         <AppointmentsList appointments={this.state.appointments} />
       </div>
     )
